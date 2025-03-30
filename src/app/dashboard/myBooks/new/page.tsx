975935b14dba5e4c72e9ea47eeca210588eb5fc6
maeda-gif <wakaengr00@gmail.com>
2025-03-30 14:24:22 +0900

    wa20250330

--- src/app/dashboard/myBooks/new/page.tsx
+++ src/app/dashboard/myBooks/new/page.tsx
@@ -1,17 +1,27 @@
-"use client";
+'use client';
 
 import { useState } from 'react';
-import { TextField, Button, Container, Typography, Stack } from '@mui/material';
+import { TextField, Button, Container, Typography, Stack, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 
 export default function NewBookPage() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [description, setDescription] = useState('');
+  const [bookData, setBookData] = useState({
+    title: '',
+    author: '',
+    publisher: '',
+    publishDate: '',
+    isbn: '',
+    review: '',
+    genre: '',
+  });
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleChange = (e) => {
+    setBookData({ ...bookData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ title, author, description });
-    // TODO: API連携して登録処理を実装
+    console.log('Submitted Book Data:', bookData);
+    // TODO: ここでAPIリクエストを実装
   };
 
   return (
@@ -19,36 +29,26 @@ export default function NewBookPage() {
       <Typography variant="h4" gutterBottom>
         本の登録
       </Typography>
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={2}>
-          <TextField
-            label="タイトル"
-            variant="outlined"
-            fullWidth
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <TextField
-            label="著者"
-            variant="outlined"
-            fullWidth
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-          <TextField
-            label="説明"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={4}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <Button type="submit" variant="contained" color="primary">
-            登録
-          </Button>
-        </Stack>
-      </form>
+      <Stack spacing={2} component="form" onSubmit={handleSubmit}>
+        <TextField label="書籍名" name="title" value={bookData.title} onChange={handleChange} required fullWidth />
+        <TextField label="著者" name="author" value={bookData.author} onChange={handleChange} required fullWidth />
+        <TextField label="出版社" name="publisher" value={bookData.publisher} onChange={handleChange} fullWidth />
+        <TextField label="出版日" name="publishDate" type="date" value={bookData.publishDate} onChange={handleChange} InputLabelProps={{ shrink: true }} fullWidth />
+        <TextField label="ISBN" name="isbn" value={bookData.isbn} onChange={handleChange} fullWidth />
+        <TextField label="口コミ・レビュー" name="review" value={bookData.review} onChange={handleChange} multiline rows={4} fullWidth />
+        <FormControl fullWidth>
+          <InputLabel>ジャンル</InputLabel>
+          <Select name="genre" value={bookData.genre} onChange={handleChange}>
+            <MenuItem value="fiction">フィクション</MenuItem>
+            <MenuItem value="nonfiction">ノンフィクション</MenuItem>
+            <MenuItem value="fantasy">ファンタジー</MenuItem>
+            <MenuItem value="mystery">ミステリー</MenuItem>
+          </Select>
+        </FormControl>
+        <Button type="submit" variant="contained" color="primary">
+          登録
+        </Button>
+      </Stack>
     </Container>
   );
 }
